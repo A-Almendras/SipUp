1439dfb94822979abacf4527e2f76111f3bd9ea3
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-07-26 01:31:28 -0400

    wip: modified schema objects

--- models/order.js
+++ models/order.js
@@ -3,14 +3,18 @@ const Schema = mongoose.Schema
 
 const orderSchema = new Schema(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    name: { type: String, required: true },
     phoneNumber: { type: String, required: true },
     paymentMethod: { type: String, required: true },
     pickupMethod: { type: String, required: true },
-    address: { type: String, required: true },
-    image: { type: String, required: true },
-    juice: { type: Schema.Types.ObjectId, ref: 'Juice' }
+    address: { type: String },
+    totalAmount: { type: Number, required: true },
+    juices: [
+      {
+        juice_Id: { type: Schema.Types.ObjectId, ref: 'Juice' },
+        quantity: { type: Number, required: true }
+      }
+    ]
   },
   { timestamps: true }
 )
